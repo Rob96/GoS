8d00ce7f3f80c11fa466621c74e43793da5cd685
Rob96 <Rob96@users.noreply.github.com>
2017-06-09 19:32:36 +0200

    Update ManlyRengar.lua

--- ManlyRengar.lua
+++ ManlyRengar.lua
@@ -27,290 +27,117 @@ function Rengar:LoadMenu()
   self.Menu.Combo:MenuElement({id = "ComboQ", name = "Use Q", value = true})
   self.Menu.Combo:MenuElement({id = "ComboW", name = "Use W", value = true})
   self.Menu.Combo:MenuElement({id = "ComboE", name = "Use E", value = true})
-  self.Menu.Combo:MenuElement({id = "Prio", name = "Combo Priority", drop = {"Q","W","E"}})
-  
-  
-  
+  self.Menu.Combo:MenuElement({id = "Prio", name = "Combo Ferocity Priority", drop = {"Q","W","E"}})
 
   self.Menu:MenuElement({type = MENU, id = "Clear", name = "Clear Settings"})
   self.Menu.Clear:MenuElement({id = "ClearQ", name = "Use Q", value = true})
   self.Menu.Clear:MenuElement({id = "ClearW", name = "Use W", value = true})
   self.Menu.Clear:MenuElement({id = "ClearE", name = "Use E", value = true})
 
-
   --[[Draw]]
   self.Menu:MenuElement({type = MENU, id = "Draw", name = "Drawings"})
   self.Menu.Draw:MenuElement({id = "DrawQW", name = "Draw Q/W Range", value = true})
   self.Menu.Draw:MenuElement({id = "DrawE", name = "Draw E Range", value = true})
-
-
-
-
 end
 
 function Rengar:Tick()
-
   if myHero.dead then return end
   if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_COMBO] and self.Menu.Combo.Prio:Value()== 1 then
-    self:ComboQ()
+    self:CQ() self:CE() self:CW()
     return end
-
   if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_COMBO] and self.Menu.Combo.Prio:Value() == 2 then
-    self:ComboW()
+    self:CW() self:CQ() self:CE()
     return end
-
   if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_COMBO] and self.Menu.Combo.Prio:Value() == 3 then
-    self:ComboE()
+    self:CE() self:CQ() self:CW()
     return end
-
-  if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_JUNGLECLEAR] --or 
+  if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_JUNGLECLEAR]
   then
     self:JClear()
-    
   end
-if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_LANECLEAR] 
-then
-  self:LClear()
+  if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_LANECLEAR]
+  then
+    self:LClear()
+  end
 end
-end
-
 
 function Rengar:JClear()
-
-  if self:GetValidMinion(650) == false then return end
-  for i = 1, Game.MinionCount() do
+  for i = 1, Game.MinionCount(725) do
     local minion = Game.Minion(i)
-
     if  minion.team == 300 then
-
-
-
-      if self:IsValidTarget(minion,410) and myHero.pos:DistanceTo(minion.pos) < 410 and self.Menu.Clear.ClearQ:Value() and self:CanCast(_Q) then
+      if myHero.pos:DistanceTo(minion.pos) < 410 and self.Menu.Clear.ClearQ:Value() and self:CanCast(_Q) then
         Control.CastSpell(HK_Q,minion.pos)
-
-      
+      end
+      if myHero.pos:DistanceTo(minion.pos) < 725 and self.Menu.Clear.ClearE:Value() and self:CanCast(_E) then
+        Control.CastSpell(HK_E,minion.pos)
+      end
+      if myHero.pos:DistanceTo(minion.pos) < 410 and self.Menu.Clear.ClearW:Value() and self:CanCast(_W) then
+        Control.CastSpell(HK_W)
+      end
     end
-
-    if self:IsValidTarget(minion,650) and myHero.pos:DistanceTo(minion.pos) < 650 and self.Menu.Clear.ClearE:Value() and self:CanCast(_E) then
-      Control.CastSpell(HK_E,minion.pos)
-
-
-    end
-
-
-
-    if self:IsValidTarget(minion,410) and myHero.pos:DistanceTo(minion.pos) < 410 and self.Menu.Clear.ClearW:Value() and self:CanCast(_W) then
-      Control.CastSpell(HK_W)
-
-    end
-
-
   end
-
-
-end
-
 end
 
 function Rengar:LClear()
-
-  if self:GetValidMinion(650) == false then return end
-  for i = 1, Game.MinionCount() do
+  for i = 1, Game.MinionCount(725) do
     local minion = Game.Minion(i)
-
     if  minion.team == 200 then
-
-
-
-      if self:IsValidTarget(minion,410) and myHero.pos:DistanceTo(minion.pos) < 410 and self.Menu.Clear.ClearQ:Value() and self:CanCast(_Q) then
+      if myHero.pos:DistanceTo(minion.pos) < 410 and self.Menu.Clear.ClearQ:Value() and self:CanCast(_Q) then
         Control.CastSpell(HK_Q,minion.pos)
-
-      
+      end
+      if myHero.pos:DistanceTo(minion.pos) < 725 and self.Menu.Clear.ClearE:Value() and self:CanCast(_E) then
+        Control.CastSpell(HK_E,minion.pos)
+      end
+      if myHero.pos:DistanceTo(minion.pos) < 410 and self.Menu.Clear.ClearW:Value() and self:CanCast(_W) then
+        Control.CastSpell(HK_W)
+      end
     end
-
-    if self:IsValidTarget(minion,650) and myHero.pos:DistanceTo(minion.pos) < 650 and self.Menu.Clear.ClearE:Value() and self:CanCast(_E) then
-      Control.CastSpell(HK_E,minion.pos)
-
-
-    end
-
-
-
-    if self:IsValidTarget(minion,410) and myHero.pos:DistanceTo(minion.pos) < 410 and self.Menu.Clear.ClearW:Value() and self:CanCast(_W) then
-      Control.CastSpell(HK_W)
-
-    end
-
-
   end
-
-
 end
 
-end
-function Rengar:ComboQ()
-  if _G.SDK.TargetSelector:GetTarget(1000) == false then return
+function Rengar:CQ()
+  if _G.SDK.TargetSelector:GetTarget(410) == false then self:CE() return
   end
-
-
   local qtarg = _G.SDK.TargetSelector:GetTarget(410)
-  if self.Menu.Combo.ComboQ:Value() and self:CanCast(_Q)  then
-    local castPosition = qtarg
-    self:CastQ(castPosition)
+  if self.Menu.Combo.ComboQ:Value() and self:CanCast(_Q) and qtarg ~= nil  then
+    Control.CastSpell(HK_Q, qtarg)
   end
+end
 
-  if _G.SDK.TargetSelector:GetTarget(1000) == false then return
+function Rengar:CE()
+  if _G.SDK.TargetSelector:GetTarget(725) == false then return
   end
   local etarg = _G.SDK.TargetSelector:GetTarget(725)
   if etarg ~= nil then
     if self.Menu.Combo.ComboE:Value() and self:CanCast(_E) and etarg:GetCollision(E.width,E.speed,E.delay) == 0
     then
-      local castPosition = etarg
-      self:CastE(castPosition)
-
+      Control.CastSpell(HK_E, etarg)
     end
   end
+end
 
+function Rengar:CW()
+  if _G.SDK.TargetSelector:GetTarget(410) == false then self:CE() return end
   local wtarg = _G.SDK.TargetSelector:GetTarget(410)
-  if self.Menu.Combo.ComboW:Value() and self:CanCast(_W) then
-    local castPosition = wtarg
-    self:CastW(castPosition)
-  end
-
-
-end
-
-
-
-function Rengar:ComboW()
-  if _G.SDK.TargetSelector:GetTarget(1000) == false then return
-  end
-
-  local wtarg = _G.SDK.TargetSelector:GetTarget(410)
-  if self.Menu.Combo.ComboW:Value() and self:CanCast(_W) then
-    local castPosition = wtarg
-    self:CastW(castPosition)
-  end
-
-  local qtarg = _G.SDK.TargetSelector:GetTarget(410)
-  if self.Menu.Combo.ComboQ:Value() and self:CanCast(_Q) then
-    local castPosition = qtarg
-    self:CastQ(castPosition)
-  end
-
-  local etarg = _G.SDK.TargetSelector:GetTarget(725)
-  if etarg ~= nil then
-    if self.Menu.Combo.ComboE:Value() and self:CanCast(_E) and etarg:GetCollision(E.width,E.speed,E.delay) == 0 then
-      local castPosition = etarg:GetPrediction(E.Speed, E.delay)
-      self:CastE(castPosition)
-
-
-
-    end
-
-
+  if self.Menu.Combo.ComboW:Value() and self:CanCast(_W) and wtarg ~= nil then
+    Control.CastSpell(HK_W,wtarg)
   end
 end
-
-
-
-function Rengar:ComboE()
-
-
-  local etarg = _G.SDK.TargetSelector:GetTarget(725)
-  if etarg ~= nil then
-    if self.Menu.Combo.ComboE:Value() and self:CanCast(_E) and etarg:GetCollision(E.width,E.speed,E.delay) == 0 then
-      local castPosition = etarg:GetPrediction(E.Speed, E.delay)
-      self:CastE(castPosition)
-    end
-  end
-
-
-
-  local qtarg = _G.SDK.TargetSelector:GetTarget(410)
-  if self.Menu.Combo.ComboQ:Value() and self:CanCast(_Q) then
-    local castPosition = qtarg
-    self:CastQ(castPosition)
-  end
-
-
-
-  local wtarg = _G.SDK.TargetSelector:GetTarget(410)
-  if self.Menu.Combo.ComboW:Value() and self:CanCast(_W) then
-    local castPosition = wtarg
-    self:CastW(castPosition)
-
-
-
-
-
-  end
-
-end
-
-
-function Rengar:CastQ(position)
-  if position then
-    Control.CastSpell(HK_Q, position)
-  end
-end
-
-function Rengar:CastW(position)
-  if position then
-    Control.CastSpell(HK_W, position)
-  end
-end
-
-function Rengar:CastE(position)
-  if position then
-    Control.CastSpell(HK_E, position)
-  end
-end
-
-
 
 function Rengar:Draw()
   if myHero.dead then return end
-
-
   if self.Menu.Draw.DrawQW:Value() then
     Draw.Circle(myHero.pos, 450, 1, Draw.Color(255, 255, 255, 255))
   end
   if self.Menu.Draw.DrawE:Value() then
     Draw.Circle(myHero.pos, 1000, 1, Draw.Color(255, 255, 255, 255))
   end
-
-
 end
 
-
-
-function Rengar:IsReady(spellSlot)
-  return myHero:GetSpellData(spellSlot).currentCd == 0 and myHero:GetSpellData(spellSlot).level > 0
+function Rengar:CanCast(spell)
+  return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana and Game.CanUseSpell(spell) == 0
 end
 
-
-function Rengar:CanCast(spellSlot)
-  return self:IsReady(spellSlot)
-end
-
-
-function Rengar:GetValidMinion(range)
-  for i = 1,Game.MinionCount() do
-    local minion = Game.Minion(i)
-    if  minion.team ~= myHero.team and minion.valid and minion.pos:DistanceTo(myHero.pos) < 650 then
-      return true
-    end
-  end
-  return false
-end
-
-
-
-
-function Rengar:IsValidTarget(unit,range)
-  return unit ~= nil and unit.valid and unit.visible and not unit.dead and unit.isTargetable and not unit.isImmortal and unit.pos:DistanceTo(myHero.pos) <= 620
-end
 
 function OnLoad()
   Rengar()
